78694a8f26bdaeb9b0f79eb6801b0eff29442b09
cwalbanese <631gti@gmail.com>
2020-04-14 10:23:56 -0400

    animated projects

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ navLinks.addEventListener('click', toggleMenu);
 setTimeout(function () {
   header.classList.remove('loading');
 }, 600);
+
+let elements;
+let windowHeight;
+
+function init() {
+  elements = document.querySelectorAll('.hidden');
+  windowHeight = window.innerHeight;
+}
+
+function checkPosition() {
+  for (let i = 0; i < elements.length; i++) {
+    let element = elements[i];
+    let positionFromTop = elements[i].getBoundingClientRect().top;
+
+    if (positionFromTop - windowHeight <= 0) {
+      element.classList.add('projects-animation');
+      element.classList.remove('hidden');
+    }
+  }
+}
+
+window.addEventListener('scroll', checkPosition);
+window.addEventListener('resize', init);
+
+init();
+checkPosition();
