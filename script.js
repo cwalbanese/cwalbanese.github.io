01de264f03e50db5e145e2c38906b457d4de7766
cwalbanese <631gti@gmail.com>
2020-04-22 17:03:41 -0400

    added additional animation

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ const header = document.querySelector('.box-wrapper');
 toggleMenu = () => {
   if (nav.style.height != '420px') {
     nav.style.height = '420px';
+    menuButton.src = 'images/close.svg';
+    menuButton.style.margin = '10px -5px -5px 55px';
     setTimeout(function () {
       navLinks.style.visibility = 'visible';
     }, 600);
   } else {
+    menuButton.src = 'images/menu.svg';
     navLinks.style.visibility = 'hidden';
     nav.style.height = '50px';
+    menuButton.style.margin = '0px 0px 0px 50px';
   }
 };
 
@@ -25,7 +29,7 @@ setTimeout(function () {
 elements = document.querySelectorAll('.hidden');
 windowHeight = window.innerHeight;
 
-function checkPosition() {
+function checkProjects() {
   for (let i = 0; i < elements.length; i++) {
     let element = elements[i];
     let positionFromTop = elements[i].getBoundingClientRect().top;
@@ -37,7 +41,31 @@ function checkPosition() {
   }
 }
 
-window.addEventListener('scroll', checkPosition);
-window.addEventListener('resize', init);
+window.addEventListener('scroll', checkProjects);
 
-checkPosition();
+skills = document.querySelectorAll('.off');
+
+function checkSkills() {
+  for (let i = 0; i < skills.length; i++) {
+    let skill = skills[i];
+    let positionFromTop = skills[i].getBoundingClientRect().top;
+
+    if (positionFromTop - windowHeight <= 0) {
+      skill.classList.add('skills-animation');
+      skill.classList.remove('off');
+    }
+  }
+}
+
+window.addEventListener('scroll', checkSkills);
+
+skillsLoaded = document.getElementById('skills');
+skillsLoaded.addEventListener('mouseout', checkLoaded);
+
+function checkLoaded(evt) {
+  evt.target.classList.remove('skills-animation');
+  evt.target.classList.add('loaded');
+}
+
+checkProjects();
+checkSkills();
